274150519953e307008ff7d7454fdb8ab46487df
saramahmoud2071 <89441343+saramahmoud2071@users.noreply.github.com>
2024-06-30 08:03:52 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Compiler_Project.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/main.cpp" "CMakeFiles/Compiler_Project.dir/main.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/main.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/automaton/Automaton.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Automaton.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Automaton.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/automaton/Conversions.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Conversions.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Conversions.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/automaton/State.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/State.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/State.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/automaton/Types.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Types.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Types.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/automaton/Utilities.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Utilities.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Utilities.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/creation/Constants.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/creation/Constants.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/creation/Constants.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/creation/InfixToPostfix.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/creation/InfixToPostfix.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/creation/InfixToPostfix.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/creation/LexicalRulesHandler.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/creation/LexicalRulesHandler.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/creation/LexicalRulesHandler.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/creation/ToAutomaton.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/creation/ToAutomaton.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/creation/ToAutomaton.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_one/prediction/Predictor.cpp" "CMakeFiles/Compiler_Project.dir/phase_one/prediction/Predictor.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_one/prediction/Predictor.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_two/FirstFollow.cpp" "CMakeFiles/Compiler_Project.dir/phase_two/FirstFollow.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_two/FirstFollow.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_two/Parser.cpp" "CMakeFiles/Compiler_Project.dir/phase_two/Parser.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_two/Parser.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_two/ReadCFG.cpp" "CMakeFiles/Compiler_Project.dir/phase_two/ReadCFG.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_two/ReadCFG.cpp.o.d"
+  "/home/ahmed-kamal/Projects/cpp_projects/Compiler_Project/phase_two/Table.cpp" "CMakeFiles/Compiler_Project.dir/phase_two/Table.cpp.o" "gcc" "CMakeFiles/Compiler_Project.dir/phase_two/Table.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Compiler_Project.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compiler_Project.dir/main.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/main.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Automaton.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Automaton.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Conversions.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Conversions.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/State.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/State.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Types.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Types.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Utilities.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/automaton/Utilities.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/Constants.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/Constants.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/InfixToPostfix.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/InfixToPostfix.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/LexicalRulesHandler.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/LexicalRulesHandler.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/ToAutomaton.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/creation/ToAutomaton.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_one/prediction/Predictor.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_one/prediction/Predictor.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_two/FirstFollow.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_two/FirstFollow.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_two/Parser.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_two/Parser.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_two/ReadCFG.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_two/ReadCFG.cpp.o.d"
+  "CMakeFiles/Compiler_Project.dir/phase_two/Table.cpp.o"
+  "CMakeFiles/Compiler_Project.dir/phase_two/Table.cpp.o.d"
+  "Compiler_Project"
+  "Compiler_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Compiler_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
